5597e7f4e2a81cc2ec1429f948dc6793fa40ca47
diegovd0296 <diegovd0296@gmail.com>
2018-08-30 20:33:44 +0200

    added directories

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-python Practica_3.py --target_class 0 --epochs 1
-python Practica_3.py --target_class 1 --epochs 1
-python Practica_3.py --target_class 2 --epochs 1
-python Practica_3.py --target_class 3 --epochs 1
-python Practica_3.py --target_class 4 --epochs 1
-python Practica_3.py --target_class 5 --epochs 1
-python Practica_3.py --target_class 6 --epochs 1
-python Practica_3.py --target_class 7 --epochs 1
-python Practica_3.py --target_class 8 --epochs 1
-python Practica_3.py --target_class 9 --epochs 1 --concat
+python Practica_3.py --target_class 0 --epochs 9
+python Practica_3.py --target_class 1 --epochs 9
+python Practica_3.py --target_class 2 --epochs 9
+python Practica_3.py --target_class 3 --epochs 9
+python Practica_3.py --target_class 4 --epochs 9
+python Practica_3.py --target_class 5 --epochs 9
+python Practica_3.py --target_class 6 --epochs 9
+python Practica_3.py --target_class 7 --epochs 9
+python Practica_3.py --target_class 8 --epochs 9
+python Practica_3.py --target_class 9 --epochs 9 --concat
 
 
 
